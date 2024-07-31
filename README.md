38da1f64523120b55e6af821abd3ac92c976d07f
exkludera <51145038+exkludera@users.noreply.github.com>
2024-07-31 11:00:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# cs2-force-say
+**plugin can force players to write in chat**
+
+<br>
+
+> css_forcesay TARGET MESSAGE
+>
+> css_forcesayall MESSAGE
---
+++ main.cs
@@ -0,0 +1,90 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+public class Plugin : BasePlugin
+{
+    public string Prefix = $" {ChatColors.Red}";
+
+    public override string ModuleName => "Force Say";
+    public override string ModuleVersion => "";
+    public override string ModuleAuthor => "exkludera";
+
+    public override void Load(bool hotReload)
+    {
+        AddCommand($"css_ForceSay", "", Command_ForceSay!);
+        AddCommand($"css_ForceSayAll", "", Command_ForceSayAll!);
+    }
+    public override void Unload(bool hotReload)
+    {
+        RemoveCommand($"css_ForceSay", Command_ForceSay!);
+        RemoveCommand($"css_ForceSayAll", Command_ForceSayAll!);
+    }
+
+    public void Command_ForceSay(CCSPlayerController player, CommandInfo command)
+    {
+        if (string.IsNullOrEmpty(command.ArgString))
+        {
+            command.ReplyToCommand(Prefix + "no args");
+            return;
+        }
+
+        var targetInput = command.ArgByIndex(1);
+        if (string.IsNullOrEmpty(targetInput))
+        {
+            command.ReplyToCommand(Prefix + "no target arg");
+            return;
+        }
+
+        var messageInput = command.ArgByIndex(2);
+        if (string.IsNullOrEmpty(messageInput))
+        {
+            command.ReplyToCommand(Prefix + "no message arg");
+            return;
+        }
+
+        bool foundPlayer = false;
+        CCSPlayerController targetPlayer = null!;
+
+        foreach (var target in Utilities.GetPlayers())
+        {
+            if (target.PlayerName.ToLower() == targetInput.ToLower())
+            {
+                targetPlayer = target;
+                foundPlayer = true;
+                break;
+            }
+            if (target.SteamID.ToString() == targetInput)
+            {
+                targetPlayer = target;
+                foundPlayer = true;
+                break;
+            }
+        }
+
+        if (foundPlayer) targetPlayer!.ExecuteClientCommandFromServer($"say {messageInput}");
+        else command.ReplyToCommand(Prefix + "target not found");
+    }
+
+    public void Command_ForceSayAll(CCSPlayerController player, CommandInfo command)
+    {
+        if (string.IsNullOrEmpty(command.ArgString))
+        {
+            command.ReplyToCommand(Prefix + "no args");
+            return;
+        }
+
+        var messageInput = command.ArgByIndex(1);
+        if (string.IsNullOrEmpty(messageInput))
+        {
+            command.ReplyToCommand(Prefix + "no message arg");
+            return;
+        }
+
+        foreach (var target in Utilities.GetPlayers())
+        {
+            target.ExecuteClientCommandFromServer($"say {messageInput}");
+        }
+    }
+}
